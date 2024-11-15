71d01a07a6dfa768a5dfc94d00516b4aa9a234ce
olamamarcel@gmail.com <olamamarcel@gmail.com>
2024-11-14 20:17:54 -0500

    Traitement d'une opération

---
+++ scripts/calculatrice.js
@@ -0,0 +1,26 @@
+console.log("chargement du script");
+
+function gestionDuFormulaire(event) {
+  event.preventDefault();
+
+  const firstNumberValue = 1 * document.getElementById("firstNumber").value;
+  const secondNumberValue = 1 * document.getElementById("secondNumber").value;
+
+  const operation = document.getElementById("operation").value;
+
+  let resultat = 0;
+
+  if (operation === "1") {
+    resultat = firstNumberValue + secondNumberValue;
+  }
+
+  if (operation === "2") {
+    resultat = firstNumberValue * secondNumberValue;
+  }
+
+  const operationLabel = operation === "1" ? "+" : "*";
+
+  document.getElementById(
+    "resultat"
+  ).innerHTML = `${firstNumberValue} ${operationLabel} ${secondNumberValue} = ${resultat}`;
+}
