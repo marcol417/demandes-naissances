4d1f1798094fc78fb0cc240e6e2b60fdaba65876
olamamarcel@gmail.com <olamamarcel@gmail.com>
2024-11-14 20:21:48 -0500

    Traitement d'une opération onchange

--- scripts/calculatrice.js
+++ scripts/calculatrice.js
@@ -24,3 +24,26 @@ function gestionDuFormulaire(event) {
     "resultat"
   ).innerHTML = `${firstNumberValue} ${operationLabel} ${secondNumberValue} = ${resultat}`;
 }
+
+function gestionDuneOperation() {
+  const firstNumberValue = 1 * document.getElementById("firstNumber").value;
+  const secondNumberValue = 1 * document.getElementById("secondNumber").value;
+
+  const operation = document.getElementById("operation").value;
+
+  let resultat = 0;
+
+  if (operation === "1") {
+    resultat = firstNumberValue + secondNumberValue;
+  }
+
+  if (operation === "2") {
+    resultat = firstNumberValue * secondNumberValue;
+  }
+
+  const operationLabel = operation === "1" ? "+" : "*";
+
+  document.getElementById(
+    "resultat"
+  ).innerHTML = `${firstNumberValue} ${operationLabel} ${secondNumberValue} = ${resultat}`;
+}
